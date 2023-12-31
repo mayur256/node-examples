bc823993952c368a71f0cca3aa0afeab6db54695
Mayur Upadhayay <mayur123a@gmail.com>
2024-01-01 00:06:00 +0530

    [chore]: Adds typescript support

--- attachment-download/src/server.ts
+++ attachment-download/src/server.ts
@@ -1,17 +1,22 @@
-// HTTP module to create a server
-import http, { IncomingMessage, Server, ServerResponse } from "node:http";
+import express, { Application, Response } from 'express';
+import { createServer, Server } from 'node:http';
+import path from 'node:path';
 
 const PORT = 8001;
 const HOST = "127.0.0.1";
 
-// create a local server to receive data from
-const server: Server = http.createServer((request: IncomingMessage, response: ServerResponse): void => {
-    const { url, method } = request;
-    
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.end('okay');
-});
+// Initialise the app
+const expressApp: Application = express();
 
-server.listen(PORT, HOST, (): void => {
+// Define routes
+expressApp.get("/", (req, res: Response) => {
+    res.sendFile(path.resolve("./src/index.html"));
+})
+
+// Create Server
+const appServer: Server = createServer(expressApp);
+
+// Start listening on the server
+appServer.listen(PORT, HOST, () => {
     console.log(`Server listening at ${HOST}:${PORT}`);
 });
