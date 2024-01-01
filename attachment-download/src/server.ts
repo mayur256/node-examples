69817d0abed2f76655733b8fe25ced59dd56957d
Mayur Upadhayay <mayur123a@gmail.com>
2024-01-01 18:27:09 +0530

    [chore]: Stable commit for attachment download example

--- attachment-download/src/server.ts
+++ attachment-download/src/server.ts
@@ -1,4 +1,4 @@
-import express, { Application, Response } from 'express';
+import express, { Application, Request, Response } from 'express';
 import { createServer, Server } from 'node:http';
 import path from 'node:path';
 
@@ -7,10 +7,21 @@ const HOST = "127.0.0.1";
 
 // Initialise the app
 const expressApp: Application = express();
+expressApp.use(express.static('public'));
 
 // Define routes
 expressApp.get("/", (req, res: Response) => {
-    res.sendFile(path.resolve("./src/index.html"));
+    res.sendFile(path.resolve("./src/assets/index.html"));
+});
+expressApp.get("/download-private-attachment", (req: Request, res: Response) => {
+    try {
+        if (req.headers['x-token']) {
+            return res.download("./src/assets/private.pdf");
+        }
+        res.status(400).json({ message: 'Token is missing' });
+    } catch (e: any) {
+        console.log(`Error::${e}`)
+    }
 })
 
 // Create Server
