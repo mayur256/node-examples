4b9d00b3af478f0b1e95d0f5de5307da2938bb22
Mayur Upadhayay <mayur123a@gmail.com>
2024-01-30 23:11:11 +0530

    [chore]: Serving a fallback file on root route

--- generic-app/index.js
+++ generic-app/index.js
@@ -1,10 +1,18 @@
 const express = require("express");
+const path = require("node:path");
 
 // Initialising express app
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Demo routes
+app.get("/", (req, res) => {
+    try {
+        res.sendFile(path.join(__dirname, 'index.html'));
+    } catch {
+        res.send("Fallback Data...")
+    }
+})
 app.get("/products", async (req, res) => {
     let results = [];
     try {
